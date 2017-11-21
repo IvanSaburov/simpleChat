98221327dd3d04a69c241b1c317a62297e3c05cd
Ivan <feb>
2017-11-21 23:48:42 +0500

    corrected output message

--- server/src/main/java/ru/simplechat/command/ClientsCountCommand.java
+++ server/src/main/java/ru/simplechat/command/ClientsCountCommand.java
@@ -17,7 +17,7 @@ public class ClientsCountCommand extends Command {
         StringBuilder sb = new StringBuilder("В данный момент ");
         switch (count) {
             case 1:
-                sb.append("1 клиент");
+                sb.append("подключен 1 клиент");
                 break;
             default:
                 sb.append("подключено " + count + " клиента(ов)");
